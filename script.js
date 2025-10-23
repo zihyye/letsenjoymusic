67bdea43540fbc48ec9479ac9fe8d59162838739
zihyye <yun05101@gmail.com>
2025-10-23 22:48:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ❗️❗️❗️ Apps Script 웹 앱 URL을 여기에 붙여넣으세요 ❗️❗️❗️
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzcTaBf0J6qAcCBJREYovpRJLHzWPRhigdxgb6Ml1FscLLVhB4zAtEsmYhzaMPqaWnZ/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwT7dRPxtcPJtx7redtaHlEymLy1mOKDXGnPr5oDYqbM9TqTs48PtowH9OXWrS_wtY8/exec";
 
 let frequencyChart, genreChart;
 
